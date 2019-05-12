7c2281eaa48c3b91a9d272979374bec6fb403d5d
christophe <christophe.lorenzato@lapiscine.pro>
2019-05-12 17:08:45 +0200

    view of coach and detail coach almost done

---
+++ src/AppBundle/Controller/CoachController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Coach;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CoachController extends Controller
+{
+    /**
+     * @Route("/listeCoach", name="listeCoach")
+     */
+    public function listeCoachBddAction()
+    {
+        $coach = $this->getDoctrine()
+            ->getRepository(Coach::class)
+            ->findAll();
+        return $this->render('default/listeCoach.html.twig',
+            [
+                'coach' => $coach
+            ]);
+    }
+
+    /**
+     * @Route("/detailCoach/{id}", name="detailCoach")
+     **/
+    public function coachDetailBddAction($id){
+        $singleCoach= $this->getDoctrine()
+            ->getRepository(Coach::class)
+            ->find($id);
+        return $this->render('default/detailCoach.html.twig',
+            [
+                'singleCoach' => $singleCoach,
+                'id' => $id
+            ]);
+    }
+}

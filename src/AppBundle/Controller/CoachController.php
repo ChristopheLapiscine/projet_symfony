6c85eee7c9bfb5f110105752aad05459c8c1676f
christophe <christophe.lorenzato@lapiscine.pro>
2019-05-13 22:58:08 +0200

    add CRUD for coach

--- src/AppBundle/Controller/CoachController.php
+++ src/AppBundle/Controller/CoachController.php
@@ -1,40 +1,136 @@
 <?php
 
-
 namespace AppBundle\Controller;
-
 
 use AppBundle\Entity\Coach;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Coach controller.
+ *
+ * @Route("admin/coach")
+ */
 class CoachController extends Controller
 {
     /**
-     * @Route("/listeCoach", name="listeCoach")
+     * Lists all coach entities.
+     *
+     * @Route("/", name="admin_coach_index")
+     * @Method("GET")
      */
-    public function listeCoachBddAction()
+    public function indexAction()
     {
-        $coach = $this->getDoctrine()
-            ->getRepository(Coach::class)
-            ->findAll();
-        return $this->render('default/listeCoach.html.twig',
-            [
-                'coach' => $coach
-            ]);
+        $em = $this->getDoctrine()->getManager();
+
+        $coaches = $em->getRepository('AppBundle:Coach')->findAll();
+
+        return $this->render('admin/coach/index.html.twig', array(
+            'coaches' => $coaches,
+        ));
     }
 
     /**
-     * @Route("/detailCoach/{id}", name="detailCoach")
-     **/
-    public function coachDetailBddAction($id){
-        $singleCoach= $this->getDoctrine()
-            ->getRepository(Coach::class)
-            ->find($id);
-        return $this->render('default/detailCoach.html.twig',
-            [
-                'singleCoach' => $singleCoach,
-                'id' => $id
-            ]);
+     * Creates a new coach entity.
+     *
+     * @Route("/new", name="admin_coach_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $coach = new Coach();
+        $form = $this->createForm('AppBundle\Form\CoachType', $coach);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($coach);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_coach_show', array('id' => $coach->getId()));
+        }
+
+        return $this->render('admin/coach/new.html.twig', array(
+            'coach' => $coach,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a coach entity.
+     *
+     * @Route("/{id}", name="admin_coach_show")
+     * @Method("GET")
+     */
+    public function showAction(Coach $coach)
+    {
+        $deleteForm = $this->createDeleteForm($coach);
+
+        return $this->render('admin/coach/show.html.twig', array(
+            'coach' => $coach,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing coach entity.
+     *
+     * @Route("/{id}/edit", name="admin_coach_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Coach $coach)
+    {
+        $deleteForm = $this->createDeleteForm($coach);
+        $editForm = $this->createForm('AppBundle\Form\CoachType', $coach);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_coach_edit', array('id' => $coach->getId()));
+        }
+
+        return $this->render('admin/coach/edit.html.twig', array(
+            'coach' => $coach,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a coach entity.
+     *
+     * @Route("/{id}", name="admin_coach_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Coach $coach)
+    {
+        $form = $this->createDeleteForm($coach);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($coach);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_coach_index');
+    }
+
+    /**
+     * Creates a form to delete a coach entity.
+     *
+     * @param Coach $coach The coach entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Coach $coach)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_coach_delete', array('id' => $coach->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
     }
 }
--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 {
 
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function homeBddAction()
     {
